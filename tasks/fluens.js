c2c67260366bce1add8f7e39bb6b7de75e664c1d
pkozhin <kpaul.box@gmail.com>
2014-08-10 04:47:09 +0400

    Implementation.

--- tasks/fluens.js
+++ tasks/fluens.js
@@ -1,5 +1,5 @@
 /**
-* FluensJS - v0.0.4-0.2
+* FluensJS - v0.0.5
 * Copyright (c) 2014 Pavel Kozhin
 * License: MIT, https://github.com/pkozhin/fluens.js/blob/master/LICENSE
 */
@@ -95,6 +95,9 @@ fluens.core.Composer = function(commentParser) {
 
 fluens.core.Fluens = function(model, cache, scopes, validator) {
 
+    var OPTIONS = "options",
+        DEFAULT_SCOPE = "default";
+
     var description = 'Interpolate templates with your data and inject the result to the desired location.',
         self = this;
 
@@ -107,23 +110,24 @@ fluens.core.Fluens = function(model, cache, scopes, validator) {
 
     var defaultPriority = 5;
 
-    this.initContext = function(context, contextType) {
-        var result = [], scope, options = _.pick(context, "options").options || {};
+    this.initContext = function(context, contextType, options) {
+        var result = [], scope;
 
         options = _.merge(defaultOptions, options);
 
         _.forIn(context, function(item, scopeType) {
             var phases = [], priority;
 
-            if (scopeType !== "options") {
+            if (scopeType !== OPTIONS && scopeType !== DEFAULT_SCOPE) {
                 scope = self.scopeFactory(scopeType, contextType, phases);
 
                 _.forIn(item, function(phase, phaseType) {
                     var processor = scopes.processor(scopeType, phaseType);
 
-                    phase.action = phase.action ||
+                    phase.action = _.isFunction(phase.action) ? phase.action :
                         (processor ? _.bind(processor.action, processor) : null);
-                    phase.validate = phase.validate ||
+
+                    phase.validate = _.isFunction(phase.validate) ? phase.validate :
                         (processor ? _.bind(processor.validate, processor) : null);
 
                     if (!phase.cwd) {
@@ -188,7 +192,7 @@ fluens.core.Fluens = function(model, cache, scopes, validator) {
     this.run = function(type, context, options) {
         if (!context) { throw new Error("Task '"+ type +"' is not configured."); }
 
-        var items = this.initContext(context, type);
+        var items = this.initContext(context, type, options);
 
         this.cacheContext(items);
         this.processContext(items);
@@ -209,7 +213,7 @@ fluens.core.Fluens = function(model, cache, scopes, validator) {
     };
 
     grunt.registerMultiTask('fluens', description, function(){
-        self.run(this.target, this.data, this.options());
+        self.run(this.target, this.data, this.options() || {});
     });
 };
 
@@ -330,12 +334,15 @@ fluens.core.FluensScope = function(type, contextType, phases) {
 
 fluens.core.FluensScopes = function() {
 
+    var DEFAULT_SCOPE = "default";
+
     var map = {};
 
     this.processor = function(scopeType, phaseType, processor) {
         if (!processor) {
             return map[scopeType] && map[scopeType][phaseType] ?
-                map[scopeType][phaseType] : null;
+                map[scopeType][phaseType] : (map[DEFAULT_SCOPE][phaseType] ?
+                map[DEFAULT_SCOPE][phaseType] : null);
         }
 
         if (!map[scopeType]) {
@@ -348,6 +355,7 @@ fluens.core.FluensScopes = function() {
         }
 
         map[scopeType][phaseType] = processor;
+        return undefined;
     };
 };
 
@@ -512,11 +520,8 @@ fluens.processor.FluensInjector = function(model) {
 
     this.phases = {
         inject: {
-            sources: this,
-            vendors: this,
-            styles: this,
-            namespaces: this,
-            dependencies: this
+            // "default" means that it handles phase "inject" for any scope.
+            default: this
         }
     };
 };
